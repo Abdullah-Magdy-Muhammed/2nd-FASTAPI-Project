cec2e7068e0b0a2cdf6c6ae1baf282c99eafa772
Abdullah <56775516+Zaazou@users.noreply.github.com>
2022-03-07 08:58:59 +0200

    DataBase First Commit

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 
 app = FastAPI()
 
 @app.get('/user')
 
-def index():
-    return {'data':{'name': 'abdullah'}}
+def index(limit=20):
+    return {'data':f'{limit}blog list'}
 
 
-@app.get('/about')
+@app.get('/about/{id}')
 
-def about():
-    return {'data': 'about our app'}
+async def shpw(id:int):
+    return {'data': id}
+
+class Blog(BaseModel):
+    title:str
+    body:str
+    published:Optional[bool]
+
+@app.post('/blog')
+
+def create_blog(request:Blog):
+    return {'data':f"blog is created with title as {request.title}"}
+
+'''
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=9000) 
+'''
